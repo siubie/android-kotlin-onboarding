158b77aba4e5c40b3e57785f8e9172dcbcfb9c95
Putra Prima Arhandi <putraprima@gmail.com>
2020-06-08 17:08:42 +0700

    1. Refactoring ViewHolder 2. Fixing Login and signup layout

--- app/src/main/java/id/putraprima/onboarding/ui/onboarding/OnboardingAdapter.kt
+++ app/src/main/java/id/putraprima/onboarding/ui/onboarding/OnboardingAdapter.kt
@@ -7,25 +7,39 @@ import androidx.recyclerview.widget.RecyclerView
 import id.putraprima.onboarding.R
 import kotlinx.android.synthetic.main.item_onboarding.view.*
 
-class OnboardingAdapter(var onboardingItem: List<OnboardingItem>) : RecyclerView.Adapter<OnboardingAdapter.ViewPagerViewHolder>(){
+class OnboardingAdapter(var onboardingItem: List<OnboardingItem>) :
+    RecyclerView.Adapter<OnboardingAdapter.ViewPagerViewHolder>() {
 
-    inner class ViewPagerViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
+    class ViewPagerViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        fun bind(
+            item: OnboardingItem
+        ) {
+            itemView.txtTitleOnboarding.text = item.title
+            itemView.imgDescriptionOnboarding.setImageResource(item.image)
+            itemView.txtDescriptionOnboarding.text = item.description
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewPagerViewHolder {
+                val view =
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.item_onboarding, parent, false)
+                return ViewPagerViewHolder(view)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_onboarding,parent,false)
-        return ViewPagerViewHolder(view)
+        return ViewPagerViewHolder.from( parent)
     }
 
-    override fun getItemCount(): Int {
-        return onboardingItem.size
-    }
+    override fun getItemCount() = onboardingItem.size
 
     override fun onBindViewHolder(holder: ViewPagerViewHolder, position: Int) {
         val item = onboardingItem.get(position)
-        holder.itemView.txtTitleOnboarding.text= item.title
-        holder.itemView.imgDescriptionOnboarding.setImageResource(item.image)
-        holder.itemView.txtDescriptionOnboarding.text = item.description
+        holder.bind(item)
     }
+
+
 }
